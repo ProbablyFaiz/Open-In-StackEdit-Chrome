b50d3fc9e0706553a9a2c9e36bc1f4ef0d383f93
Faiz Surani <faiz.surani@astera.com>
2019-07-24 21:10:26 -0700

    Dev work on regex match url

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-// Copyright 2019 Faiz Surani
+// Copyright 2019 Faiz Surani, MIT License
 
 'use strict';
 
@@ -6,7 +6,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {urlMatches: 'git(hub|lab)\.com\/[^/]*\/[^/]*\/tree\/[^/]*\/.*'},
+        pageUrl: {urlMatches: 'git(hub|lab)\.com\/[^/]*\/[^/]*\/tree\/master'},
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
@@ -14,5 +14,5 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.pageAction.onClicked.addListener( function(tabs.Tab tab) {
-	
+	let tabUrl = tab.url;
 });
