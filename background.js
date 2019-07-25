294b6a76a934e504c3b1653df2c1cdc52507672a
Faiz Surani <faiz.surani@astera.com>
2019-07-24 21:58:05 -0700

    Working extension and icons
    Will now submit

--- background.js
+++ background.js
@@ -6,13 +6,25 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {urlMatches: 'git(hub|lab)\.com\/[^/]*\/[^/]*\/tree\/master'},
+        pageUrl: {urlMatches: 'git(hub|lab)\.com\/[^/]*\/[^/]*\/tree\/.*'},
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 });
 
-chrome.pageAction.onClicked.addListener( function(tabs.Tab tab) {
+chrome.pageAction.onClicked.addListener( function(tab) {
+	chrome.extension.getBackgroundPage().console.log('foo');
 	let tabUrl = tab.url;
+	let regEx = new RegExp('https:\\/\\/git(hub|lab)\\.com\\/([^/]*)\\/([^/]*)\\/tree\\/([^/]*)\\/(.*)');
+	let match = tabUrl.match(regEx);
+
+	let gitProvider = match[1];
+	let repoOwner = match[2];
+	let repoName = match[3];
+	let branchName = match[4];
+	let path = match[5].replace('/', '%2F');
+
+	let stackEditUrl = 'https://stackedit.io/app#providerId=git' + gitProvider + 'Workspace&owner=' + repoOwner + '&repo=' + repoName + '&branch=' + branchName + '&path=' + path;
+	chrome.tabs.create({ url: stackEditUrl });
 });
